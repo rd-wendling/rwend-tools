d0501ae2b2e42e38a02019f98d8eed0819259fcd
Ryan Wendling <ryan.wendling@state.co.us>
2024-04-11 10:57:59 -0600

    updated idis_processing to include file type option and output file naming Tim wanted

--- doh_tools/idis_processing.py
+++ doh_tools/idis_processing.py
@@ -1,28 +1,39 @@
-#%%
 import pandas as pd
 import os
 import zipfile
+import re
 
 def process_idis_fwf(filename):
     '''
     Process the IDIS FWF Export from HUD.
 
-    This function takes a fixed-width IDIS file, and coverts it to a pandas df. 
+    This function takes a fixed-width IDIS file, and converts it to a pandas DataFrame.
 
     Parameters:
         - filename: Path to the fixed-width IDIS file
 
     Returns:
-        - Pandas df
+        - Pandas DataFrame
     '''
     # Define an empty list to store column specifications
     col_specs = []
 
     # Read the fixed-width file
     with open(filename, 'r') as file:
-        # Skip the first 5 lines
-        for _ in range(5):
-            next(file)
+        # Skip line 1
+        next(file)
+        
+        # Read in line 2
+        table_name1 = file.readline().strip()
+
+        # Skip Line 3
+        next(file)
+
+        # Read in line 4 as table name
+        table_name2 = file.readline().strip()
+
+        # Skip line 5
+        next(file)
 
         # Read the file line by line
         for line in file:
@@ -49,6 +60,13 @@ def process_idis_fwf(filename):
         # Read the data into a DataFrame using the extracted column specifications
         df = pd.read_fwf(file, colspecs=[(spec[0]-1, spec[1]) for spec in col_specs], names=[spec[2] for spec in col_specs])
 
+        # Add the TABLE_NAME column to the DataFrame
+        table_name = table_name1 + ' ' + table_name2
+        table_name = table_name.replace('*', '')
+        table_name = re.sub(r'\s+', ' ', table_name).strip()
+        table_name = table_name.replace(' ', '_').lower()
+        df['TABLE_NAME'] = table_name
+
     return df
 
 
@@ -73,18 +91,19 @@ def extract_zip(zip_path):
 
 
 
-def idis_fwf_to_csv(input_path, output_folder_path):
+def idis_fwf_to_file(input_path, output_folder_path, output_format='csv'):
     '''
     Process the IDIS FWF Export from HUD.
 
-    This function takes a path to a folder with FWF IDIS files or zipped folders with said files, and writes them out as CSVs to a defined output folder. 
+    This function takes a path to a folder with FWF IDIS files or zipped folders with said files, and writes them out as CSVs or Parquet files to a defined output folder.
 
     Parameters:
         - input_path: Path to the fixed-width IDIS files and/or zipped folders
-        - output_folder_path: Path to the folder you want the csvs written out to
+        - output_folder_path: Path to the folder you want the files written out to
+        - output_format: Output format, either 'csv' or 'parquet'. Default is 'csv'.
 
     Returns:
-        - All content that was fwfs as csvs
+        - All content that was fwfs as csvs or parquet files
     '''
     if not os.path.exists(input_path):
         print("The path does not exist.")
@@ -95,15 +114,20 @@ def idis_fwf_to_csv(input_path, output_folder_path):
         if os.path.isfile(item_path):
             if item.lower().endswith('.txt'):
                 df = process_idis_fwf(item_path)
-                out_item = os.path.splitext(item)[0] + '.csv'
-                out_path = os.path.join(output_folder_path, out_item)
-                df.to_csv(out_path, index=False)
+                out_item = df['TABLE_NAME'][0]
+                if output_format == 'csv':
+                    out_path = os.path.join(output_folder_path, out_item + '.csv')
+                    df.to_csv(out_path, index=False)
+                elif output_format == 'parquet':
+                    out_path = os.path.join(output_folder_path, out_item + '.parquet')
+                    df.to_parquet(out_path, index=False)
+                else:
+                    print("Unsupported output format. Please choose either 'csv' or 'parquet'.")
             elif zipfile.is_zipfile(item_path):
                 extract_zip(item_path)
                 extract_path = os.path.join(input_path, os.path.splitext(item)[0])
-                idis_fwf_to_csv(extract_path, output_folder_path)
+                idis_fwf_to_file(extract_path, output_folder_path, output_format)
             else:
                 print(f"{item} is not a text file or zipped folder, skipping.")
         elif os.path.isdir(item_path):
-            idis_fwf_to_csv(item_path, output_folder_path)
-
+            idis_fwf_to_file(item_path, output_folder_path, output_format)
