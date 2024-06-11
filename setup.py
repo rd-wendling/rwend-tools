7f751ea56f1bc9899e170da44c3ac8aae1daa6e8
RyanW <rwendling17@gmail.com>
2024-06-11 16:32:51 -0600

    added req packages so they auto install

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 #%%
-from setuptools import setup
+
+from setuptools import setup, find_packages
 
 setup(
-    name='rwend-tools',
-    version='1.0',
+    name='rwend_tools',
+    version='1.3',
     packages=['rwend_tools'],
+    install_requires=[
+        'pyyaml',
+        'googlemaps',
+        'google-cloud-secret-manager'
+    ],
+    # Other metadata
+    author='Ryan Wendling',
+    url='https://github.com/rd-wendling/rwend-tools',
 )
